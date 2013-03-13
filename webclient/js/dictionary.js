27d6fd0d932e272d49aa4440022aa230a6600b2c
OpenShift guest <kythyria@berigora.net>
2013-03-12 23:18:55 -0400

    Add a simple little dictionary (JS lacks one as such)

---
+++ webclient/js/dictionary.js
@@ -0,0 +1,46 @@
+define([],function(){
+    return function()
+    {
+        var store = {};
+        var keys = [];
+        
+        var decorate = function(key)
+        {
+            return "+$$" + key.toString();
+        }
+        
+        var get = function(key)
+        {
+            return store[decorate(key)];
+        };
+        
+        var set = function(key, value)
+        {
+            if(keys.indexOf(key)==-1) keys.push(key);
+            return store[decorate(key)] = value;
+        };
+        
+        var forEach = function(fn)
+        {
+            keys.forEach(function(i){
+                fn(i, get(i));
+            });
+        };
+        
+        var remove = function(key)
+        {
+            var idx = keys.indexOf(key);
+            if (idx == -1) return;
+            
+            keys.splice(idx,1);
+            var removed = get(key);
+            delete store[decorate(key)];
+            return removed;
+        };
+        
+        this.get = get;
+        this.set = set;
+        this.forEach = forEach;
+        this.remove = remove;
+    };
+});
